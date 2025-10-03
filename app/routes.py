52e23cbee9c5445e6a8f034b39b596a927c8fc5d
Salim Hemed <milas.dev@gmail.com>
2025-10-04 00:29:22 +0200

    Defined Landing Page

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,27 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,render_template
+from app import db
+from app.models import Patient, Visit
 # from app.oidc import oidc
 
 bp = Blueprint("routes", __name__)
 
 @bp.route("/")
 def index():
-    return {"msg": "Daycare App Running"}
+    total_patients = db.session.query(Patient).count()
+    total_visits = db.session.query(Visit).count()
+    recent_visits = (
+        Visit.query.order_by(Visit.visit_date.desc().nullslast())
+        .limit(5)
+        .all()
+    )
+
+    return render_template(
+        "index.html",
+        total_patients=total_patients,
+        total_visits=total_visits,
+        recent_visits=recent_visits,
+    )
+    
 
 # @bp.route("/private")
 # @oidc.oidc_auth("default")
